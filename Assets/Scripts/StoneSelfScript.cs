a82b25b6eaad7fdb112c8f0a94204e221f29a8f6
TimWang\Administrator <timowang1991@yahoo.com.tw>
2015-01-03 23:19:00 +0800

    just for testing let Giant lost HP

--- Assets/Scripts/StoneSelfScript.cs
+++ Assets/Scripts/StoneSelfScript.cs
@@ -1,48 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ArrowSelfScript : MonoBehaviour {
-
-	public enum ArrowState
-	{
-		holding,
-		shooted,
-		touched
-	}
-	public ArrowState state;
+public class StoneSelfScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		state = ArrowState.holding;
-		this.gameObject.transform.localScale = new Vector3(1f,1f,1f);
-		Destroy (this.gameObject, 10);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (state == ArrowState.shooted) {
-			if(transform.localScale.x < 30)
-			{
-				transform.localScale += new Vector3 (1F,1F,1F);
-			}
-		}
+	
 	}
 
 	void OnTriggerEnter(Collider other) {
-
+		
 		if(other.gameObject.tag == "Weak")
 		{
-			Debug.Log ("Arrow: " + other.gameObject.tag);
+			Debug.Log ("Stone: " + other.gameObject.tag);
 			GameObject.FindGameObjectWithTag("GiantPlayer").GetComponent<GiantHealth>().HurtGiant(1,this.gameObject.tag);
 		}
 		else if(other.gameObject.tag == "Weaker")
 		{
-			Debug.Log ("Arrow: " + other.gameObject.tag);
+			Debug.Log ("Stone: " + other.gameObject.tag);
 			GameObject.FindGameObjectWithTag("GiantPlayer").GetComponent<GiantHealth>().HurtGiant(2,this.gameObject.tag);
 		}
 		else if(other.gameObject.tag == "Weakest")
 		{
-			Debug.Log ("Arrow: " + other.gameObject.tag);
+			Debug.Log ("Stone: " + other.gameObject.tag);
 			GameObject.FindGameObjectWithTag("GiantPlayer").GetComponent<GiantHealth>().HurtGiant(3,this.gameObject.tag);
 		}
 	}
