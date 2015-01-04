46f0138c48b79c19a8ba73cdfd52eae621d8fa48
jessehsiu <jesse801122@gmail.com>
2015-01-05 00:11:56 +0800

    add Stone explosionFX

--- Assets/Scripts/StoneSelfScript.cs
+++ Assets/Scripts/StoneSelfScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class StoneSelfScript : MonoBehaviour {
 
 	public PhotonView CatapultPhotonView;
-
+	public GameObject ExplosionFX;
 	// Use this for initialization
 	void Start () {
 
@@ -25,20 +25,24 @@ public class StoneSelfScript : MonoBehaviour {
 				Debug.Log ("Stone: " + other.gameObject.tag);
 				GameObject.FindGameObjectWithTag("GiantPlayer").GetComponent<GiantHealth>().HurtGiant(1,this.gameObject.tag);
 				PhotonNetwork.Destroy(this.gameObject);
+				Instantiate (ExplosionFX, other.contacts[0].point, Quaternion.identity);
 			}
 			else if(other.gameObject.tag == "Weaker")
 			{
 				Debug.Log ("Stone: " + other.gameObject.tag);
 				GameObject.FindGameObjectWithTag("GiantPlayer").GetComponent<GiantHealth>().HurtGiant(2,this.gameObject.tag);
 				PhotonNetwork.Destroy(this.gameObject);
+				Instantiate (ExplosionFX, other.contacts[0].point, Quaternion.identity);
 			}
 			else if(other.gameObject.tag == "Weakest")
 			{
 				Debug.Log ("Stone: " + other.gameObject.tag);
 				GameObject.FindGameObjectWithTag("GiantPlayer").GetComponent<GiantHealth>().HurtGiant(3,this.gameObject.tag);
 				PhotonNetwork.Destroy(this.gameObject);
+				Instantiate (ExplosionFX, other.contacts[0].point, Quaternion.identity);
 			}
 
 		}
+
 	}
 }
