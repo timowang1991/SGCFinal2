72d26a0ebaf54a26a4f730eb37a872ab69f8e917
TimWang\Administrator <timowang1991@yahoo.com.tw>
2015-02-24 12:50:31 +0800

    Add magician prefab and some new grab and release files

---
+++ Assets/Scripts/ObjectDisappear.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDisappear : Photon.MonoBehaviour {
+	public float timerToDestroy = 30f;
+	bool isPhotonSelfDestruct = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Called by GrabAndReleaseTree, distory self after "timerToDestroy" using RPC to tell everyone to destory.
+	/// </summary>
+	public void countDownDestroy(){
+		if (isPhotonSelfDestruct == false ) {
+			Invoke ("PhotonSelfDestruct", timerToDestroy);
+			isPhotonSelfDestruct = true;
+		}
+	}
+	/// <summary>
+	/// Called by GrabAndReleaseTree, cancel the destory if the tree hasn't destory.
+	/// </summary>
+	public void cancelDestroy(){
+		if (isPhotonSelfDestruct == true) {
+			CancelInvoke ("PhotonSelfDestruct");
+			isPhotonSelfDestruct = false;
+		}
+	}
+	
+	/// <summary>
+	/// Destory now!!![using RPC]
+	/// </summary>
+	void PhotonSelfDestruct(){
+		//photonView.RPC("selfTreeDestroy", PhotonTargets.All, null);
+		Debug.Log ("PhotonSelfDestruct");
+		PhotonNetwork.Destroy(this.gameObject);
+		
+	}
+}
