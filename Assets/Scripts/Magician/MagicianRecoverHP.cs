84e4e13bc063eccec3eaf60a1b6cbfb1d8daa1e4
jessehsiu <jesse801122@gmail.com>
2015-02-25 10:23:28 +0800

    Add magician files

---
+++ Assets/Scripts/Magician/MagicianRecoverHP.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicianRecoverHP : MonoBehaviour {
+	
+	public float CostMP = 10;
+	public int DefaultRecoveryHPvalue = 5;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	public void CastSpell(int photonViewId)
+	{
+		if (this.gameObject.GetComponent<MagicianMPManager> ().UseMP (CostMP)) {
+			PhotonView.Find(photonViewId).RPC("RecoverHP", PhotonTargets.All, DefaultRecoveryHPvalue);
+		}
+	}
+}
