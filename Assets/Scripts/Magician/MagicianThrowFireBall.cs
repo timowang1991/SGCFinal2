a96dd28375b36393062d37558ebd554418dc205a
jessehsiu <jesse801122@gmail.com>
2015-03-15 15:09:44 +0800

    delete duplicate photon on giant

--- Assets/Scripts/Magician/MagicianThrowFireBall.cs
+++ Assets/Scripts/Magician/MagicianThrowFireBall.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class MagicianThrowFireBall : Photon.MonoBehaviour {
 
 	public int CostMP = 10; 
+	public string FireBall_Prefab = "FireBall_Magician_Net";
+	private Transform camTrans;
 
-	public void CastSpell(int photonViewId)
+	void Start()
 	{
+		if (FireBall_Prefab == null) {
+			Debug.LogError("NO FireBall_Prefab Loaded");
+		}
+		camTrans = Camera.main.transform;
+	}
+
+	public void CastSpell()
+	{
+
 		if (this.gameObject.GetComponent<MagicianMPManager> ().UseMP (CostMP)) {
 			this.photonView.RPC("FireTheFireBall",PhotonTargets.All);
 		}
@@ -14,6 +25,10 @@ public class MagicianThrowFireBall : Photon.MonoBehaviour {
 	[RPC]
 	void FireTheFireBall()
 	{
+		CapsuleCollider cc = this.gameObject.collider as CapsuleCollider;
+		Vector3 v3 = camTrans.forward / camTrans.forward.magnitude;
+		GameObject fireBall = PhotonNetwork.Instantiate (FireBall_Prefab, this.gameObject.transform.position, Quaternion.identity, 0);
+		fireBall.transform.LookAt ( camTrans.position,Vector3.forward);
 		Debug.Log(this.photonView.viewID + " is Firing the Fire ball");
 	}
 
---
+++ Assets/Scripts/Object/FireBallSelfScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallSelfScript : MonoBehaviour {
+
+
+	public float timeToDestory = 10;
+	// Use this for initialization
+	void Start () {
+		Invoke ("selfDestory", timeToDestory);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void selfDestory()
+	{
+		Destroy (this.gameObject);
+	}
+}
