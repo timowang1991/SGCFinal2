3358e21fc0f29d3c985bc1e150a9b33e0fbc6d46
jessehsiu <jesse801122@gmail.com>
2015-04-11 14:27:54 +0800

    add DropdownPackages (From fish)

---
+++ Assets/Scripts/DropdownController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropdownController : Photon.MonoBehaviour {
+	
+	//public Transform packagePrefab;
+	public string packagePrefab = "Package";
+	Platform platform;
+	
+
+	// Use this for initialization
+	void Start () {
+		platform = GameObject.Find("PlatformManager").GetComponent<PlatformIndicator>().platform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (/*platform == Platform.PC_Giant &&*/ Input.GetKeyDown(KeyCode.P) ) {
+			Drop ();
+
+		}
+	}
+
+	void Drop(){
+		Vector3 packagePosition = new Vector3 (Random.Range (170, 330), 120, Random.Range (270, 370));
+		GameObject package = PhotonNetwork.Instantiate (packagePrefab, packagePosition, Quaternion.identity, 0);
+
+	}
+
+}
---
+++ Assets/Scripts/Package.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Package : MonoBehaviour {
+
+	public float timeToDestory = 10;
+	public int addHP = 10;
+	// Use this for initialization
+	void Start () {
+		Invoke ("selfDestory", timeToDestory);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		print (col.gameObject.tag);
+		if (col.gameObject.tag == "Player") {
+			print (col.gameObject.name);
+			//add HP by sending negative value to damage function
+			col.gameObject.GetComponent<HealthSystem>().damage(-addHP);
+			//PhotonView.Destroy(gameObject);
+			PhotonNetwork.Destroy(gameObject);
+		}
+		
+	}
+
+
+}
+
