ecb20dcb6945739d8f82d9ee2a43135c6ed317c8
KanTei <r03944003@ntu.edu.tw>
2015-04-16 18:56:35 +0800

    fix bugs like wind,leaves and dust effect, and rocket,bullets destroy scripts

---
+++ Assets/bulletCollidedBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using PathologicalGames;
+
+public class bulletCollidedBehaviour : MonoBehaviour {
+
+	private bool isCollided = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionExit(Collision collision) {
+		if (!isCollided) {
+			isCollided = true;
+			Invoke ("despawnSelf", 2);
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (!isCollided) {
+			isCollided = true;
+			Invoke ("despawnSelf", 2);
+		}
+	}
+	
+	void despawnSelf() {
+		PoolManager.Pools [GameConfig.CopterPoolName].Despawn (gameObject.transform); //remove rocket
+		isCollided = false;
+	}
+	
+}
