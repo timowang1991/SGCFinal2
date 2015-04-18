afcdf1fef90012557bab6f29b26ab231e0542bca
TimWang\Administrator <timowang1991@yahoo.com.tw>
2015-04-18 22:26:40 +0800

    GiantCircularProgress only works when cloning the two eye canvas

--- Assets/Scripts/Giant/GiantCircularProgressEventCreator.cs
+++ Assets/Scripts/Giant/GiantCircularProgressEventCreator.cs
@@ -1,16 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GiantCircularProgressAndHandOverlapMgr : MonoBehaviour {
+public class GiantCircularProgressEventCreator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
 }
