7ef7bf41ae7e3d9402cfaa04fc4e29e3a319a418
TimWang\Administrator <timowang1991@yahoo.com.tw>
2015-04-20 01:41:12 +0800

    hurt laser by interval

--- Assets/Scripts/HurtByLaser.cs
+++ Assets/Scripts/HurtByLaser.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class HurtByLaser : MonoBehaviour {
 
+	public float timeInterval = 1.0f;
+	private float nowTime;
 	public int hurtValue = 30;
+
+
+	void Start()
+	{
+		nowTime = Time.time;
+	}
 
 	[RPC]
 	void hurtByLaser()
 	{
-		this.GetComponent<HealthSystem>().damage(hurtValue);
-		this.GetComponent<Fire>().caughtFire();
+		if(Time.time > nowTime)
+		{
+			this.GetComponent<HealthSystem>().damage(hurtValue);
+			this.GetComponent<Fire>().caughtFire();
+			nowTime = nowTime + timeInterval;
+		}
 	}
 }
