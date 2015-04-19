ead1b5996f98a9325498f11fcc22f657a2dca6d1
jessehsiu <jesse801122@gmail.com>
2015-04-20 00:17:48 +0900

    new Scene by LJW (All scenes changed!)

--- Assets/Helicopter Pack Black Gear studio/Standard Assets/Image Effects (Pro Only)/CameraMotionBlur.js
+++ Assets/Helicopter Pack Black Gear studio/Standard Assets/Image Effects (Pro Only)/CameraMotionBlur.js
@@ -99,7 +99,7 @@ public class CameraMotionBlur extends PostEffectsBase
 		CheckSupport (true, true); // depth & hdr needed
 		motionBlurMaterial = CheckShaderAndCreateMaterial (shader, motionBlurMaterial);
 
-		if (supportDX11 && filterType == MotionBlurFilter.ReconstructionDX11) {
+		if (filterType == MotionBlurFilter.ReconstructionDX11) {
 			dx11MotionBlurMaterial = CheckShaderAndCreateMaterial (dx11MotionBlurShader, dx11MotionBlurMaterial);
 		}
 	
--- Assets/_CharacterSystem/Tutorial_Assets/Standard Assets/Image Effects (Pro Only)/SepiaToneEffect.cs
+++ Assets/_CharacterSystem/Tutorial_Assets/Standard Assets/Image Effects (Pro Only)/SepiaToneEffect.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
-namespace UnitySampleAssets.ImageEffects
-{
-    [ExecuteInEditMode]
-    [AddComponentMenu("Image Effects/Sepia Tone")]
-    public class SepiaToneEffect : ImageEffectBase
-    {
+[ExecuteInEditMode]
+[AddComponentMenu("Image Effects/Sepia Tone")]
+public class SepiaToneEffect : ImageEffectBase {
 
-        // Called by camera to apply image effect
-        private void OnRenderImage(RenderTexture source, RenderTexture destination)
-        {
-            Graphics.Blit(source, destination, material);
-        }
-    }
+	// Called by camera to apply image effect
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+		Graphics.Blit (source, destination, material);
+	}
 }
