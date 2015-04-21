278731ff84381814be625ff3459552917f8b3cfe
jessehsiu <jesse801122@gmail.com>
2015-04-21 19:15:42 +0900

    let 360 camera work perfectly

--- Assets/Scripts/Camera360.cs
+++ Assets/Scripts/Camera360.cs
@@ -3,49 +3,53 @@ using System.Collections;
 
 public class Camera360 : MonoBehaviour {
 
-	public GameObject middlePoint;
-	public int speed = 20;
+	private Transform originalPoint;
+	public Transform middlePoint;
+	public int rotateSpeed = 20;
+	public int moveSpeed = 50;
 	// Use this for initialization
 	void Start () {
-	
+		originalPoint = middlePoint;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.LookAt (middlePoint.transform.position);
-		transform.RotateAround(middlePoint.transform.position, Vector3.down, speed * Time.deltaTime);
+		Vector3 tmpPoint = Vector3.Lerp (originalPoint.position, middlePoint.position, Time.deltaTime);
+
+		this.transform.LookAt (tmpPoint);
+		transform.RotateAround(tmpPoint, Vector3.down, rotateSpeed * Time.deltaTime);
 
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			if(Input.GetKey(KeyCode.LeftShift))
 			{
-				this.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+				this.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
 			}
 			else
 			{
-				this.transform.Translate(Vector3.up * Time.deltaTime * speed,Space.World);
+				this.transform.Translate(Vector3.up * Time.deltaTime * moveSpeed,Space.World);
 			}
 		}
 		else if (Input.GetKey(KeyCode.DownArrow))
 		{
 			if(Input.GetKey(KeyCode.LeftShift))
 			{
-				this.transform.Translate(Vector3.back * Time.deltaTime * speed);
+				this.transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
 			}
 			else
 			{
-				this.transform.Translate(Vector3.down * Time.deltaTime* speed,Space.World);
+				this.transform.Translate(Vector3.down * Time.deltaTime* moveSpeed,Space.World);
 			}
 
 		}
 
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			this.transform.Translate(Vector3.right * Time.deltaTime* speed,Space.World);
+			this.transform.Translate(Vector3.right * Time.deltaTime* moveSpeed,Space.World);
 		}
 		else if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			this.transform.Translate(Vector3.left * Time.deltaTime* speed,Space.World);
+			this.transform.Translate(Vector3.left * Time.deltaTime* moveSpeed,Space.World);
 		}
 	}
 }
