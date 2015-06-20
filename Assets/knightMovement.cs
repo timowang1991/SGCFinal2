373b6f28e2a419f356702183ce2e14845671678a
Juliexoxo <juliechenxoxo@gmail.com>
2015-06-20 16:03:43 +0800

    animation and movement bug fixed

--- Assets/knightMovement.cs
+++ Assets/knightMovement.cs
@@ -6,12 +6,12 @@ public class knightMovement : Photon.MonoBehaviour {
 	//public Animator knightAnim;
 	public CNAbstractController leftJoystick;
 	public CNAbstractController rightJoystick;
-	Vector3 movement;
 	Rigidbody playerRigidbody;
-	public float speed; 
 	Animator anim; 
-	
-	
+	Vector3 movement;
+	public float speed; 
+	public float turnSmoothing = 15f;
+
 	// Use this for initialization
 	void Start () {
 		// Set up references.
@@ -22,21 +22,16 @@ public class knightMovement : Photon.MonoBehaviour {
 		
 	}
 	
-	void Awake ()
-	{
-		// Create a layer mask for the floor layer.
-		//floorMask = LayerMask.GetMask ("Floor");
-		
-		
-	}
+
 	void Update()
 	{
 		float h = leftJoystick.GetAxis ("Horizontal");
 		float v = leftJoystick.GetAxis ("Vertical");
 		float rh = rightJoystick.GetAxis ("Horizontal");
 		float rv = rightJoystick.GetAxis ("Vertical");
-		//UpdateRotateMovement ();
+
 		UpdateMovement (h,v);
+		UpdateRotateMovement (h,v);
 		UpdateAnimation (h, v, rh, rv);
 		
 	}
@@ -44,29 +39,49 @@ public class knightMovement : Photon.MonoBehaviour {
 	void UpdateMovement (float h, float v) {
 		// Set the movement vector based on the axis input.
 		movement.Set (h, 0f, v);
-		speed = h;
+		speed = movement.normalized.magnitude;
 		// Normalise the movement vector and make it proportional to the speed per second.
-		movement = movement.normalized * speed *10* Time.deltaTime;
-		
+		if (v < 0) {
+			movement = movement.normalized * -speed * 30 * Time.deltaTime;
+		} else {
+			movement = movement.normalized * speed * 30 * Time.deltaTime;
+		}
 		// Move the player to it's current position plus the movement.
 		playerRigidbody.MovePosition (transform.position + movement);
+	}
+	void UpdateRotateMovement (float h,float v)
+	{
+		// Create a new vector of the horizontal and vertical inputs.
+		Vector3 targetDirection = new Vector3(h, 0f, v);
+		
+		// Create a rotation based on this new vector assuming that up is the global y axis.
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		
+		// Create a rotation that is an increment closer to the target rotation from the player's rotation.
+		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+		
+		// Change the players rotation to this new rotation.
+		rigidbody.MoveRotation(newRotation);
+
 	}
 	void UpdateAnimation (float h, float v, float rh, float rv)
 	{
 		// Create a boolean that is true if either of the input axes is non-zero.
-		bool walking = h != 0f || v != 0f;
-		
-		
+		bool walking = v != 0f || h != 0f;
+		//bool turning = h > 0.5f;
+		bool direction = v > 0.1f;
+		bool running = v != 0f || h != 0f;//v > 0.8f || v < -0.8f;
+	
 		// Tell the animator whether or not the player is walking.
 		anim.SetBool ("isWalking", walking);
-		Debug.Log ("h"+h);
-		Debug.Log ("v"+v);
-		anim.SetFloat ("speed", speed);
-		Debug.Log ("speed"+speed);
-		anim.SetFloat ("turningSpeed", 0);
-		if (rh > 0)
-		{
-			anim.SetTrigger("attack");
+		//anim.SetBool ("isTurning", turning);
+		anim.SetBool ("direction", direction);
+		anim.SetBool ("isRunning", running);
+		anim.SetFloat ("turningSpeed", 0f);
+		if (rv > 0) {
+				anim.SetTrigger ("attack");
+		} else if (rv < 0) {
+				anim.SetTrigger ("shield");
 		}
 		
 		//anim.SetTrigger ("jump", jump);
